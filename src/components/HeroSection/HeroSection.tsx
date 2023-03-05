3219ab1b658b1f0c6bf957a789976c2d3bcc0eca
at235am <at235am@gmail.com>
2023-03-05 02:45:38 -0800

    Created an autoplay feature with a pause functionality to cycle through the sections.

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,20 +1,26 @@
 "use client";
+import clsx from "clsx";
 import { useHasMounted } from "@/hooks/useHasMounted";
 import { Tab } from "@headlessui/react";
-import clsx from "clsx";
-import {
-  AnimatePresence,
-  AnimationProps,
-  motion,
-  useScroll,
-  useTransform,
-} from "framer-motion";
-import Image from "next/image";
 import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { HumanDisclosure } from "../HumanDisclosure/HumanDisclosure";
 import { PodcasterDisclosure } from "../PodcasterDisclosure/PodcasterDisclosure";
 import { ScientistDisclosure } from "../ScientistDisclosure/ScientistDisclosure";
 import { TeacherDisclosure } from "../TeacherDisclosure/TeacherDisclosure";
+import {
+  IconPlayerPlayFilled,
+  IconPlayerStopFilled,
+} from "@tabler/icons-react";
+import {
+  AnimatePresence,
+  AnimationProps,
+  motion,
+  useAnimationControls,
+  useScroll,
+  useTransform,
+} from "framer-motion";
+
+const DURATION = 5000;
 
 type SectionType = {
   id: string;
@@ -36,12 +42,56 @@ const animProps: AnimationProps = {
 
 export const HeroSection = () => {
   const [section, setSection] = useState(0);
+  const [pauseSlider, setPauseSlider] = useState(false);
   const sectionSelected = section >= 0 && section <= 3;
+  const mounted = useHasMounted();
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout = setTimeout(() => {}, 0);
+
+    if (!pauseSlider) {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        setSection((v) => (v + 1) % sections.length);
+      }, DURATION);
+    }
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [section, pauseSlider]);
 
   return (
     <section className="-z-10 relative flex flex-col min-h-[calc(100vh-4rem)] mb-96 sm:mb-64 md:mb-40 lg:mb-0">
       <MountedPlayback />
       <div className="pack-content flex flex-col justify-center flex-1">
+        <div className="flex flex-col pb-16">
+          <button
+            type="button"
+            onClick={() => setPauseSlider((v) => !v)}
+            className="w-full flex flex-col text-zinc-500 hover:text-zinc-300 focus-visible:text-zinc-300"
+          >
+            <span className="flex gap-1 items-center ml-1 font-extralight text-sm uppercase">
+              <span className="">
+                {pauseSlider ? (
+                  <IconPlayerPlayFilled size={16} />
+                ) : (
+                  <IconPlayerStopFilled size={16} />
+                )}
+              </span>
+              <span className="">
+                {pauseSlider
+                  ? "stopped · click to autoplay"
+                  : "autoplaying · click to stop"}
+              </span>
+            </span>
+            <TimerLine
+              section={section}
+              duration={DURATION}
+              paused={pauseSlider}
+            />
+          </button>
+        </div>
         {/* 
           Using grid here instead of flex to solve two issues:
             1. On mobile, the two main containers are stacked on top of each other. 
@@ -52,7 +102,7 @@ export const HeroSection = () => {
         */}
         <div className="relative grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-32">
           <Tab.Group vertical selectedIndex={section} onChange={setSection}>
-            <Tab.List className="z-10 relative flex flex-col">
+            <Tab.List className="z-10 relative flex flex-col focus-within:ring-1 focus-within:focus-visible:ring-white/50 ring-white/0">
               <Orb
                 delay={0}
                 className="-z-10 absolute top-[-10%] left-[-10%] w-60 h-60 rounded-full blur-3xl bg-yellow-500 opacity-10"
@@ -62,11 +112,14 @@ export const HeroSection = () => {
                 className="-z-10 absolute top-[50%] left-[10%] w-96 h-72 rounded-full blur-3xl bg-pink-500 opacity-5"
               />
               {sections.map((sec, i) => (
-                <Tab key={sec.id} className="flex outline-none appearance-none">
+                <Tab
+                  key={sec.id}
+                  className="flex outline-none appearance-none zzfocus-visible:underline"
+                >
                   {section === i && (
                     <motion.span
-                      initial={{ scale: 0.6 }}
-                      animate={{ scale: 0.6 }}
+                      initial={{ scale: 0.6, x: mounted ? 50 : 0 }}
+                      animate={{ scale: 0.6, x: 0 }}
                       layoutId="hero-dot"
                       className="h-full aspect-square bg-zinc-50 rounded-full"
                     />
@@ -144,7 +197,7 @@ const BackgroundPlayback = () => {
       >
         <div className="scale-[calc(16/9)] md:scale-100">
           <div className="flex aspect-square items-center md:aspect-video transition-[opacity] opacity-40">
-            {/* <iframe
+            <iframe
               tabIndex={-1}
               className="w-full h-full pointer-events-none"
               width="426"
@@ -154,7 +207,7 @@ const BackgroundPlayback = () => {
               title="YouTube video player"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               // allowfullscreen
-            ></iframe> */}
+            ></iframe>
           </div>
         </div>
         <div className="absolute -inset-[3px] bg-gradient-to-b from-transparent via-zinc-900/50 zzvia-transparent to-zinc-900"></div>
@@ -196,5 +249,39 @@ const Orb = ({
         delay,
       }}
     />
+  );
+};
+
+const TimerLine = ({
+  section,
+  duration,
+  paused,
+}: {
+  section: number;
+  duration: number;
+  paused: boolean;
+}) => {
+  const controls = useAnimationControls();
+
+  useEffect(() => {
+    if (!paused) {
+      controls.set({ width: "0%" });
+      controls.start({
+        width: "100%",
+        transition: { ease: "linear", duration: duration / 1000 },
+      });
+    } else {
+      controls.stop();
+      controls.start({
+        width: "0%",
+        transition: { ease: "linear", duration: 0.2 },
+      });
+    }
+  }, [section, duration, paused]);
+
+  return (
+    <div className="w-full h-px bg-white/5">
+      <motion.div className="h-full bg-zinc-700/80" animate={controls} />
+    </div>
   );
 };
